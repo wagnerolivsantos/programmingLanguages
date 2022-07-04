0cf07cc998b05f25e91a8218b0fad85a8c278950
Wagner Oliveira <wagnerolivsantosdev@gmail.com>
2022-07-03 21:29:13 -0300

    🚀 Added Type of Number

---
+++ JavaScript/Code/typeNumber.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const myNumber = 5;
+const firstNumber = 1;
+const secondNumber = 2;
+const floatingPoint = 10.45;
+
+const mathematicalOperation = myNumber - (firstNumber + secondNumber);
+
+console.log(mathematicalOperation);
+
+// NaN -> Not A Number
+const text = "alura";
+
+console.log(text * myNumber);
+
